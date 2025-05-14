dddba932e6577ba53fd573cf6c560ecd41adb9df
CoceraCia <coceraciamiguel@gmail.com>
2025-05-14 11:13:01 +0200

    si

--- view/index.php
+++ view/index.php
@@ -257,34 +257,6 @@ if (!isset($_SESSION["activeUser"])) {
       </div>
     </div>
   </div>
-  <div class="carrousel-mobile-body">
-    <div class="carrousel-mobile-container">
-      <div>
-        <div class="carrousel-mobile-content">
-          <h2>Jane Doe</h2>
-          <span>UI & UX Designer</span>
-        </div>
-      </div>
-      <div>
-        <div class="carrousel-mobile-content">
-          <h2>Alex Smith</h2>
-          <span>CEO Expert</span>
-        </div>
-      </div>
-      <div>
-        <div class="carrousel-mobile-content">
-          <h2>Emily New</h2>
-          <span>Web Designer</span>
-        </div>
-      </div>
-      <div>
-        <div class="carrousel-mobile-content">
-          <h2>Lisa Boley</h2>
-          <span>Marketing Coordinator</span>
-        </div>
-      </div>
-    </div>
-
 
   </div>
 
